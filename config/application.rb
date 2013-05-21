67b13bd102421f4b692542ea8bdaf119195cb246
Andrew <flashbulb54@gmail.com>
2013-05-21 12:32:27 +0400

    assets

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Turclub
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
